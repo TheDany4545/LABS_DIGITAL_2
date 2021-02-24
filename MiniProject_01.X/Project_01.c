cf7a4e824cbd3108052ae5a655a7f6da7e844a51
TheDany4545 <erickdanielaquino4545@gmail.com>
2021-02-23 23:32:48 -0600

    FINAL

--- MiniProject_01.X/Project_01.c
+++ MiniProject_01.X/Project_01.c
@@ -11,62 +11,48 @@
 #include <xc.h>
 
 
-//******************************************************************************
-//CONFIGURACION
-//******************************************************************************
-// CONFIG1
-#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator: Crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
-#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
-#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
-#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
-#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
-#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
-
-// CONFIG2
-#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
-#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
-
-//#define _XTAL_FREQ 8000000
-
-//******************************************************************************
-//VARIABLES
-//******************************************************************************
-
-//******************************************************************************
-//PROTOTIPOS DE FUNCIONES
-//******************************************************************************
-void setup(void);
-
-
-//******************************************************************************
-//CICLO PRINCIPAL
-//******************************************************************************
 
 void main(void) {
+    
+    OSCCONbits.IRCF =0b110;
+    OSCCONbits.OSTS= 0;
+    OSCCONbits.HTS = 0;
+    OSCCONbits.LTS = 0;
+    OSCCONbits.SCS = 1;
+    
+    ANSEL = 0b000000000;
+    ANSELH = 0b00000000;
+    
+    SDO = 0;
+    SDI = 1;
+    SCK = 0;
+    SS = 0;
+    CONTADOR = 0;
+    CONTADORPROVICIONAL = 0;
+    BANDERA =0;
+    
+    PORTA = 0b00000000;
+    PORTB = 0b00000000;
+    PORTC = 0b00000000;
+    PORTD = 0b00000000;
+    PORTE = 0b0000;
+    UART_Init(9600);
+    spiMasterInit();
+    
+    
+    
+    
+    
+    while(1){
+        __delay_ms(10);
+        spiWrite(0);
+        masterIn = spiRead();
+        UARTR();
+        UARTW(masterIn);
+        //UARTR();
+        //PORTD = uartIn;
+        PORTB = uartIn;
+        
 
-    setup();
-
-    //**************************************************************************
-    //LOOP PRINCIPAL
-    //**************************************************************************
-
-    while (1) {
-
-    }
+    }    
 }
-
-//******************************************************************************
-//CONFIGURACION
-//******************************************************************************
-
-void setup(void) {
-
-}
-
-//******************************************************************************
-//FUNCIONES
-//******************************************************************************
