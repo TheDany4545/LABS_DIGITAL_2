a7329212c0b7b58c76a18dabfc8f32197e532bc7
TheDany4545 <erickdanielaquino4545@gmail.com>
2021-04-27 07:25:17 -0600

    2 jugadores

---
+++ Videojuego_Galaga/Sounds/Defeat/Defeat.ino
@@ -0,0 +1,338 @@
+/**
+ * MIDI-Arduino Sequencing code generator
+ * Courtesy of http://greenlightgo.org/projects/midi-to-arduino/
+ **/
+
+#define msPerTick 1
+
+void setup(){
+
+  doPerformance();
+}
+
+void loop(){
+}
+
+void doPerformance(){
+
+  delay(4 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(2 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(3 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+
+  delay(1 * msPerTick);
+
+}
